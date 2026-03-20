80eee46056aa4974a5afd1a9256c403685e3cc29
André Guilherme <75867486+Wolf3s@users.noreply.github.com>
2026-03-19 22:48:53 -0300

    Disable clang format in rcode

--- modules/pademu/ds34bt.c
+++ modules/pademu/ds34bt.c
@@ -1013,8 +1013,9 @@ static int L2CAP_event_task(int result, int bytes)
                 pad = MAX_PADS;
             }
         } // acl_handle_ok
-    } // !rcode
-
+// clang-format off
+    }    // !rcode
+// clang-format on
     return pad;
 }
 
