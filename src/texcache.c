8f5446269307a03270bd5ce1123aed772f572c7c
Wolf3s <andregui17@outlook.com>
2026-05-09 13:40:44 -0300

    Improvements on caching cover art.

--- src/texcache.c
+++ src/texcache.c
@@ -38,9 +38,9 @@ static void cacheLoadImage(void *data)
     GSTEXTURE *texture = &req->entry->texture;
     texFree(texture);
 
-    if (handler->itemGetImage(handler, req->cache->prefix, req->cache->isPrefixRelative, req->value, req->cache->suffix, texture, GS_PSM_CT24) >= 0)
+    if (handler->itemGetArchivedImage(handler, req->cache->prefix, req->value, req->cache->suffix, texture, GS_PSM_CT24) >= 0)
         req->entry->lastUsed = guiFrameId;
-    else if (handler->itemGetArchivedImage(handler, req->cache->prefix, req->value, req->cache->suffix, texture, GS_PSM_CT24) >= 0)
+    else if (handler->itemGetImage(handler, req->cache->prefix, req->cache->isPrefixRelative, req->value, req->cache->suffix, texture, GS_PSM_CT24) >= 0)
         req->entry->lastUsed = guiFrameId;
     else
         req->entry->lastUsed = 0;
